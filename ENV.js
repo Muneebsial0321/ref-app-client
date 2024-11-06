b5a6984e29444cb1eda9ed146dba942d51f87eb8
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-11-06 19:33:43 +0500

    backend integrated

--- ENV.js
+++ ENV.js
@@ -1,3 +1,1 @@
-export const BACK_URL_ = 'https://api.teqtak.com/backend2'
-export const BACK_URL___ = 'http://13.61.4.214'
-export const BACK_URL = 'http://localhost:3001'
+export const BACK_URL = 'https://api.reffaralmoney.com'
